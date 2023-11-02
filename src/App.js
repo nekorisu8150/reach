f63770a91060eea2b40b81431220eaf9b039db7a
nekorisu8150 <70683813+nekorisu8150@users.noreply.github.com>
2023-11-02 19:05:28 +0900

    エンコード変更、スピードダイアル追加

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import "./styles.css";
+import React from "react";
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+
+// アイコン関連
+import Icon from '@mdi/react';
+import { mdiSyllabaryHiragana } from '@mdi/js';
+import { mdiIdeogramCjk } from '@mdi/js';
+
+const actions = [
+    { icon: <Icon path={mdiIdeogramCjk} size={3} />, name: '書き', key: 'Write' },
+    { icon: <Icon path={mdiSyllabaryHiragana} size={3} />, name: '読み', key: 'Read' },
+];
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+    return (
+        <div className="App">
+            <SpeedDial
+                ariaLabel="SpeedDial basic example"
+                sx={{ position: 'absolute', bottom: 30, right: 16 }}
+                icon={<SpeedDialIcon />}
+            >
+                {actions.map((action) => (
+                    <SpeedDialAction
+                        key={action.key}
+                        icon={action.icon}
+                        tooltipTitle={action.name}
+                    />
+                ))}
+            </SpeedDial>
+            <h1>Hello CodeSandbox</h1>
+            <h2>Start editing to see some magic happen!</h2>
+        </div>
+    );
 }
