ae29b01d481f280a225d9fa67e4e441145370cd5
nekorisu8150 <nekorisu8150@example.com>
2023-12-01 07:42:40 +0900

    RubyTextコンポーネント整理

--- src/compornents/rubyText.js
+++ src/compornents/rubyText.js
@@ -1,22 +1,50 @@
-import React, { Fragment } from "react";
+import React from "react";
 
 const RubyText = (props) => {
     const { list, startIndex } = props;
 
+    // 問題箇所でないテキスト
+    const getNoRbText = (str) => {
+        return str;
+    };
+
+    // 書き問題箇所のテキスト
+    const getRbWrite = (str) => {
+        return <rb className="rb-write">{str}</rb>;
+    };
+
+    // 読み問題箇所のテキスト
+    const getRbRead = (str) => {
+        return <rb className="rb-read">{str}</rb>;
+    };
+
+    // 書き問題箇所のルビ
+    const getRtWrite = (str) => {
+        return <rt className="rt-write">{str}</rt>;
+    };
+
+    // 読み問題箇所のルビ
+    const getRtRead = (str) => {
+        return <rt className="rt-read">{str}</rt>;
+    };
+
     return (
-        <Fragment>
+        <>
             {list.map((item, index) => (
                 <p key={item.key} className="text">
                     {index + startIndex + ". "}
                     {item.text.map((_item, _index) => (
                         <ruby key={_item.key}>
-                            {_item.body}
-                            {(_item.yomi !== "") && <rt>{_item.yomi}</rt>}
+                            {(_item.yomi === "") && getNoRbText(_item.body)}
+                            {(_item.yomi !== "") && (item.mode === 0) && (
+                                <>{getRbWrite(_item.body)}{getRtWrite(_item.yomi)}</>)}
+                            {(_item.yomi !== "") && (item.mode === 1) && (
+                                <>{getRbRead(_item.body)}{ getRtRead(_item.yomi)}</>)}
                         </ruby>
                     ))}
                 </p>
             ))}
-        </Fragment>
+        </>
     );
 };
 
